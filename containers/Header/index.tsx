41f07d26214f89cb520c292cfaefd089672c5078
Mateus Cardoso <serjumano17@gmail.com>
2022-03-23 21:28:13 -0300

    feat!: added fontawesome icons

--- containers/Header/index.tsx
+++ containers/Header/index.tsx
@@ -1,8 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+
 const Header = () => {
   return (
-    <header className="w-full h-16 bg-transparent flex flex-row">
+    <header className="w-full h-16 py-1.5 pbg-transparent flex flex-row">
       <section></section>
-      <section></section>
+      <section>
+        <FontAwesomeIcon icon={faInstagram} width='25px' height='25px'/>
+      </section>
     </header>
   )
 }
