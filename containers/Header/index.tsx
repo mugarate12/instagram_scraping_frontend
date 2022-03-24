0a081350d4c528e889af81c6a792c4014b23b7e3
Mateus Cardoso <serjumano17@gmail.com>
2022-03-23 21:18:22 -0300

    feat!: tailwnd added

---
+++ containers/Header/index.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="w-full h-16 bg-transparent flex flex-row">
+      <section></section>
+      <section></section>
+    </header>
+  )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import {
+  Header
+} from './../containers'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <>
+      <Header />
+
+      <Component {...pageProps}/>
+  </>
+  )
 }
 
 export default MyApp
