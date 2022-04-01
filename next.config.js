c061f0e75670721d006f8fa283dd9ff69ca14163
Mateus Cardoso <serjumano17@gmail.com>
2022-03-31 22:54:13 -0300

    feat!: added dotenv to env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require('dotenv')
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  }
 }
 
 module.exports = nextConfig
